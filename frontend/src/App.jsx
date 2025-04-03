42a5b58caece7e59e2baace759a71f9e6df79d40
wessam mustafa <wessam.2003.ali@gmail.com>
2025-04-03 15:08:05 +0300

    ui design completted
    pages home - signup - login - post - notifications - profile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,29 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from './components/common/RightPanel';
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <p>
-        Hello, World!
-      </p>
-    </>
-  );
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/Notification' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
+			</Routes>
+      <RightPanel />
+		</div>
+	);
 }
 
 export default App;
